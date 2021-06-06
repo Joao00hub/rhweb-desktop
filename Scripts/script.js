05bb24c1ad78aee2331a17f62c9e560aa1f77bbc
João Tadeu <joao_full@yahoo.com>
2021-06-06 18:28:02 -0300

    Criação do arquivo script.js e funcionalidade de consulta ao cep

---
+++ Scripts/script.js
@@ -0,0 +1,11 @@
+let endereco = "";
+
+async function pegarEndereco(){
+    let cep = document.getElementById('txtCep').value;
+    let resposta = await fetch('http://viacep.com.br/ws/' + cep + '/json/')
+    endereco = await resposta.json();
+    console.log(endereco);
+    document.getElementById('txtEnde').value = endereco.logradouro;
+    document.getElementById('txtCid').value = endereco.localidade;
+    document.getElementById('txtBair').value = endereco.bairro;
+    }
