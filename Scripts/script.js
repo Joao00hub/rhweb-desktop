0bfb5bd66909a36fd37b649238ded7a5edf3fa45
lohannalucia <lohannalucia55@gmail.com>
2021-06-21 21:42:12 -0300

    Refatoração pg cadastro de funcionários
    Ajustes de layout para padronização.
    Correções de chamadas de função fora do front.

--- Scripts/script.js
+++ Scripts/script.js
@@ -9,3 +9,36 @@ async function pegarEndereco(){
     document.getElementById('txtCid').value = endereco.localidade;
     document.getElementById('txtBair').value = endereco.bairro;
     }
+
+async function checkGraduationFields(){
+    if(!document.getElementById('grad_ok').checked){
+        document.getElementById('nomeCurso').disabled = true
+    }else{
+        document.getElementById('nomeCurso').disabled = false
+    }
+}
+
+async function checkDefFields(){
+    if(!document.getElementById('defSim').checked){
+        document.getElementById('defTipo').disabled = true
+    }else{
+        document.getElementById('defTipo').disabled = false
+    }
+}
+
+async function setPicture(){
+    var preview = document.querySelector('img');
+    var file    = document.querySelector('input[type=file]').files[0];
+    var reader  = new FileReader();
+  
+    reader.onloadend = function () {
+      preview.src = reader.result;
+    }
+  
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      preview.src = "";
+    }
+  }
+
